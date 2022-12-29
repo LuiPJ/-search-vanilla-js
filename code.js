28f6778dfe35a6afeb0a37d2dbfda226576f2455
LuiPJ <mister.keyn@mail.ru>
2022-12-29 15:11:20 +0300

    feat: fix search input

--- code.js
+++ code.js
@@ -1,12 +1,10 @@
 const searchInput = document.querySelector('.search');
 const searchOptions = document.querySelector('.options');
-
 const api = `https://gist.githubusercontent.com/VasilyMur
 /43ef6df83bba694f871f11a16ed7556d/raw/
 b6edff674e35452d6c57ec64177a558f7adb432e/moscowSubway.json`;
 
 const stations = [];
-console.log();
 
 fetch(api)
   .then((res) => res.json())
@@ -16,7 +14,7 @@ fetch(api)
     });
   });
 
-function getOptions(word = 'oleg', stations) {
+function getOptions(word, stations) {
   return stations.filter((station) => {
     const regex = new RegExp(word, 'gi');
     return station.name.match(regex);
@@ -24,8 +22,6 @@ function getOptions(word = 'oleg', stations) {
 }
 
 function displayOptions() {
-  console.log(this.value);
-
   const options = getOptions(this.value, stations);
   const html = options
     .map((station) => {
@@ -41,5 +37,5 @@ function displayOptions() {
   searchOptions.innerHTML = this.value ? html : null;
 }
 
-searchInput.addEventListener('change', displayOptions);
+searchInput.addEventListener('input', displayOptions);
 searchInput('keyup', displayOptions);
